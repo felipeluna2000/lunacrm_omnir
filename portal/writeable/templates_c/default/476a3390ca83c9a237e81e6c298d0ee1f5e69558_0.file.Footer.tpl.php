318013a10207bd61e6493928235224152b201420
Jim D <root@chezsez.com>
2026-01-28 18:18:31 +0000

    Added Customer Portal

---
+++ portal/writeable/templates_c/default/476a3390ca83c9a237e81e6c298d0ee1f5e69558_0.file.Footer.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.39, created on 2026-01-28 18:06:05
+  from '/var/www/sites/praesto/portal/layouts/default/templates/Footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_697a500d2f2534_38893507',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '476a3390ca83c9a237e81e6c298d0ee1f5e69558' => 
+    array (
+      0 => '/var/www/sites/praesto/portal/layouts/default/templates/Footer.tpl',
+      1 => 1752039698,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_697a500d2f2534_38893507 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+</div>
+</body>
+</html>
+<?php }
+}
